7ce43e6e7449316cf33a22cd50657390dba0abf7
Silviu <silviurosu1988@gmail.com>
2023-01-12 12:56:26 +0100

    populated home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import Home from "containers/pages/Home";
 import store from "./store";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Cases from "containers/pages/Cases";
+import Services from "containers/pages/Services";
+import About from "containers/pages/About";
+import Careers from "containers/pages/Careers";
+import Blog from "containers/pages/Blog";
+import Contact from "containers/pages/Contact";
 
 
 function App() {
@@ -15,6 +21,12 @@ function App() {
 
           {/* Home */}
           <Route path="/" element={<Home />} />
+          <Route path="/casos" element={<Cases />} />
+          <Route path="/servicios" element={<Services />} />
+          <Route path="/nosotros" element={<About />} />
+          <Route path="/carreras" element={<Careers />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contacto" element={<Contact />} />
         </Routes>
       </Router>
     </Provider>
--- src/containers/pages/Home.jsx
+++ src/containers/pages/Home.jsx
@@ -1,13 +1,14 @@
 import Layout from 'hocs/layouts/Layout'
 import Navbar from 'components/navigation/Navbar'
 import Footer from 'components/navigation/Footer'
+import Header from 'components/home/Header'
 
 function Home() {
   return (
     <Layout>
       <Navbar/>
       <div className='pt-28'>
-        Home
+        <Header/>
       </div>
       <Footer/>
     </Layout>
