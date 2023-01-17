580131a9ea90d1b89920ce3d90cc0760ccbb4b67
Silviu <silviurosu1988@gmail.com>
2023-01-17 10:37:12 +0100

    added shadow animation to navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
         "orange-button": "#FF561e",
       }
     },
+    boxShadow: {
+      navbar: "0px 10px 8px 0px rgba(3,3,4,0.03), 0px 1px 2px -1px rgba(3,3,4,0.03)",
+    }
   },
   plugins: [
     function ({ addUtilities }) {
