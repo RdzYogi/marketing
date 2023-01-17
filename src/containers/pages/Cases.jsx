bd03b1b6c4752bb8575f3d2f0bf7f43493bdc69d
Silviu <silviurosu1988@gmail.com>
2023-01-17 11:15:55 +0100

    adjusted header components

--- src/containers/pages/Cases.jsx
+++ src/containers/pages/Cases.jsx
@@ -1,13 +1,14 @@
 import Layout from 'hocs/layouts/Layout'
 import Navbar from 'components/navigation/Navbar'
 import Footer from 'components/navigation/Footer'
+import Header from 'components/cases/Header'
 
 function Cases() {
   return (
     <Layout>
       <Navbar/>
       <div className='pt-28'>
-        Cases
+        <Header/>
       </div>
       <Footer/>
     </Layout>
