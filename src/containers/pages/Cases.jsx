55eaea25405d62b0a0980a744d3f60edd35df968
Silviu <silviurosu1988@gmail.com>
2023-01-17 12:29:38 +0100

    installed react carousel

--- src/containers/pages/Cases.jsx
+++ src/containers/pages/Cases.jsx
@@ -2,6 +2,7 @@ import Layout from 'hocs/layouts/Layout'
 import Navbar from 'components/navigation/Navbar'
 import Footer from 'components/navigation/Footer'
 import Header from 'components/cases/Header'
+import CasesList from 'components/cases/CasesList'
 
 function Cases() {
   return (
@@ -9,6 +10,7 @@ function Cases() {
       <Navbar/>
       <div className='pt-28'>
         <Header/>
+        <CasesList/>
       </div>
       <Footer/>
     </Layout>
