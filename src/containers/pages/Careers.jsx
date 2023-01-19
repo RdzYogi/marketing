3dd65b68cfd34dcff96db0bdb4c9e511f8307ebb
Silviu <silviurosu1988@gmail.com>
2023-01-19 10:42:04 +0100

    Added header to careers

--- src/containers/pages/Careers.jsx
+++ src/containers/pages/Careers.jsx
@@ -2,6 +2,7 @@ import Layout from 'hocs/layouts/Layout'
 import Navbar from 'components/navigation/Navbar'
 import Footer from 'components/navigation/Footer'
 import { useEffect } from 'react'
+import Header from 'components/careers/Header'
 
 function Careers() {
   useEffect(() => {
@@ -11,7 +12,7 @@ function Careers() {
     <Layout>
       <Navbar/>
       <div className='pt-28'>
-        Careers
+        <Header/>
       </div>
       <Footer/>
     </Layout>
