a74b1d1051d30043099e0d8a13e98c15cbdaff80
Silviu <silviurosu1988@gmail.com>
2023-01-31 12:37:13 +0100

    Get for blog posts

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
 
-# Create your views here.
+from .models import Post
+from .serializers import PostSerializer
+from apps.category.models import Category
+
+class BlogListView(APIView):
+    permission_classes = [permissions.AllowAny,]
+    def get(self, request, format=None):
+        if Post.objects.all().exists():
+            posts = Post.objects.all()
+            serializer = PostSerializer(posts, many=True)
+            return Response({'posts': serializer.data }, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'No posts found'}, status=status.HTTP_404_NOT_FOUND)
