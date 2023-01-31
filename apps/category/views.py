24a8b15147528a15c3a83ccf821b5b7e696ebff3
Silviu <silviurosu1988@gmail.com>
2023-01-31 12:02:43 +0100

    get reponse for categories

--- apps/category/views.py
+++ apps/category/views.py
@@ -12,9 +12,30 @@ class ListCategoriesView(APIView):
     def get(self, request, format=None):
         if Category.objects.all().exists():
             categories = Category.objects.all()
-            # return Response(categories)
-            print('Categories List')
-            return Response({'categories': 'Test msg'},status=status.HTTP_200_OK)
+
+            result = []
+
+            for category in categories:
+                if not category.parent:
+                    item = {}
+                    item['id'] = category.id
+                    item['name'] = category.name
+                    item['slug'] = category.slug
+                    item['views'] = category.views
+
+                    item['subcategories'] = []
+                    for sub_cat in categories:
+                        sub_item = {}
+                        if sub_cat.parent and sub_cat.parent.id == category.id:
+                            sub_item['id'] = sub_cat.id
+                            sub_item['name'] = sub_cat.name
+                            sub_item['slug'] = sub_cat.slug
+                            sub_item['views'] = sub_cat.views
+                            item['subcategories'].append(sub_item)
+
+                    result.append(item)
+
+            return Response({'categories': result},status=status.HTTP_200_OK)
         else:
             return Response({'error': 'No categories found'},status=status.HTTP_404_NOT_FOUND)
 
